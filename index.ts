4d45a588fc2d51855c6aea3bcf4ea8eae59a65fb
Tshireletso <tmogwere09@gmail.com>
2024-06-06 18:00:28 +0200

    added number types notes

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
-// Number Types mini-challenge 10 10.2
+// Number Types mini-challenge 10 10.2 (takes full intergers/floats)
 // Write a function that will only accept numbers and attend to 
 // all TypeScript weakness flags.
-// : number
+// : number (syntax to set number parameter)
 const reviewTotalDisplay = document.querySelector('#reviews')
 
 const reviews = [
