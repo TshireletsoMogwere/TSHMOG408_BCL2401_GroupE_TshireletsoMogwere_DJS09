c4036a461217d726430db55e98bab53239ac66e1
Tshireletso <tmogwere09@gmail.com>
2024-06-07 20:57:23 +0200

    created utils

---
+++ utills.ts
@@ -0,0 +1,17 @@
+const reviewTotalDisplay = document.querySelector('#reviews')
+const userNameDisplay = document.querySelector('#user')
+const returningUserDisplay = document.querySelector('#returning-user')
+
+function showReviewTotal(value: number, reviewer: string, isLoyalty: boolean) {
+    const iconDisplay = isLoyalty ? '⭐' : ''
+    reviewTotalDisplay.innerHTML = 'review total ' + value.toString() + '| last reviewed by ' + reviewer + ' ' + iconDisplay
+}
+
+function populateUser(isReturning : boolean, userName: string ) {
+    if (isReturning == true){
+        returningUserDisplay.innerHTML = 'back'
+    }
+    userNameDisplay.innerHTML = userName
+}
+
+export default showReviewTotal; populateUser
