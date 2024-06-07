e4e08706a3ec632a8ad397e62554bb861511b3f5
Tshireletso <tmogwere09@gmail.com>
2024-06-07 20:37:34 +0200

    assigns types to paramters to change welcome message

--- index.ts
+++ index.ts
@@ -3,6 +3,8 @@
 // all TypeScript weakness flags.
 // : number (syntax to set number parameter)
 const reviewTotalDisplay = document.querySelector('#reviews')
+const userNameDisplay = document.querySelector('#user')
+const returningUserDisplay = document.querySelector('#returning-user')
 
 const reviews = [
     {
@@ -32,3 +34,17 @@ function showReviewTotal (value : number, reviewer: string, isLoyalty : boolean)
 }
 
 showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser)
+
+
+const you = {
+    userName: 'Bobby',
+    isReturning: true,
+}
+
+function populateUser(isReturning : boolean, userName : string ) {
+    if (isReturning){
+        returningUserDisplay.innerHTML = 'back'
+    }
+    userNameDisplay.innerHTML = userName
+}
+populateUser(you.isReturning, you.userName)
