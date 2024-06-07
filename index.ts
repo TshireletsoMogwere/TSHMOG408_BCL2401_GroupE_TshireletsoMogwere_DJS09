95ecb50577feadcc91b4f2deb395e48c583f6747
Tshireletso <tmogwere09@gmail.com>
2024-06-07 20:31:39 +0200

    displays most recent reviewers

--- index.ts
+++ index.ts
@@ -26,8 +26,8 @@ const reviews = [
 ]
 
 // Function to display total number of reviews
-function showReviewTotal (value : number) {
-    reviewTotalDisplay.innerHTML = 'review total ' + value.toString()
+function showReviewTotal (value : number, reviewer: string) {
+    reviewTotalDisplay.innerHTML = 'review total ' + value.toString() + '| last reviewed by' + reviewer
 }
 
-showReviewTotal(reviews.length)
+showReviewTotal(reviews.length, reviews[0].name)
