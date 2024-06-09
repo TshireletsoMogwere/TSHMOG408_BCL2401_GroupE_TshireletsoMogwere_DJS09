fafa69b38115998ee0f4cd077baa3eac7b0c56c8
Tshireletso <tmogwere09@gmail.com>
2024-06-09 11:56:47 +0200

    replaces values of loyaltyUser

--- index.ts
+++ index.ts
@@ -20,19 +20,19 @@ const reviews : {
     {
         name: 'Sheia', 
         stars: 5, //integer
-        loyaltyUser: true, //boolean
+        loyaltyUser: LoyaltyUser.GOLD_USER, //boolean
         date: '01-04-2021' //string
     },
     {
         name: 'Andrzej',
         stars: 3,
-        loyaltyUser: false,
+        loyaltyUser: LoyaltyUser.BRONZE_USER,
         date: '28-03-2021'
     },
     {
         name: 'Omar',
         stars: 4,
-        loyaltyUser: true,
+        loyaltyUser: LoyaltyUser.SILVER_USER,
         date: '27-03-2021'
     },
 ]
