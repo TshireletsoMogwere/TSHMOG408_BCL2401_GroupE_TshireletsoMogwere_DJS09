a2839484ca2b41e619538179acdc5c69732eacef
Tshireletso <tmogwere09@gmail.com>
2024-06-09 11:35:28 +0200

    fixed export error

--- utils.ts
+++ utils.ts
@@ -2,16 +2,15 @@ const reviewTotalDisplay = document.querySelector('#reviews')
 const userNameDisplay = document.querySelector('#user')
 const returningUserDisplay = document.querySelector('#returning-user')
 
-function showReviewTotal(value: number, reviewer: string, isLoyalty: boolean) {
+export function showReviewTotal(value: number, reviewer: string, isLoyalty: boolean) {
     const iconDisplay = isLoyalty ? '⭐' : ''
     reviewTotalDisplay.innerHTML = 'review total ' + value.toString() + '| last reviewed by ' + reviewer + ' ' + iconDisplay
 }
 
-function populateUser(isReturning : boolean, userName: string ) {
+export function populateUser(isReturning : boolean, userName: string ) {
     if (isReturning == true){
         returningUserDisplay.innerHTML = 'back'
     }
     userNameDisplay.innerHTML = userName
 }
 
-export default showReviewTotal; populateUser
