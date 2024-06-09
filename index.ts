cb89ba1e3374e8f73d7579685809bd16ac6de1cd
Tshireletso <tmogwere09@gmail.com>
2024-06-09 12:39:49 +0200

    adds description to omar

--- index.ts
+++ index.ts
@@ -11,12 +11,7 @@ const propertyContainer = document.querySelector('.properties')
 const footer = document.querySelector('.footer')
 let isOpen : boolean
 
-const reviews : {
-    name: string;
-    stars: number;
-    loyaltyUser: boolean;
-    date: string;
-}[] = [
+const reviews : any[] = [
     {
         name: 'Sheia', 
         stars: 5, //integer
@@ -33,7 +28,8 @@ const reviews : {
         name: 'Omar',
         stars: 4,
         loyaltyUser: LoyaltyUser.SILVER_USER,
-        date: '27-03-2021'
+        date: '27-03-2021',
+        description: 'Great hosts, location was a bit further than said',
     },
 ]
 
